e2e642fecb74312edcca76f4dacb76bb044e74d7
AlexFroio <AlexFroio42@gmail.com>
2016-03-15 16:12:58 -0500

    Vital changes to Login+changes to bot behaviour

--- src/main.py
+++ src/main.py
@@ -6,9 +6,10 @@ user_agent = ("JokerPractice v0.1 by DukeCarge")
 
 r = praw.Reddit(user_agent = user_agent)
 o = OAuth2Util.OAuth2Util(r, server_mode=True)
-r.login("BotWhoJokes", "RobotHumor42????")
+r.login("BotWhoJokes", "RobotHumor42????", disable_warning=True)
 o.refresh(force = True)
 thing_limit = 10
+botname = ["BotWhoJokes"]
 jokeWords = ["joke"]
 already_done = []
 
@@ -18,7 +19,12 @@ while True:
     for submission in subreddit.get_hot(limit=thing_limit):
         op_text = submission.selftext.lower()
         has_joke = any(string in op_text for string in jokeWords)
-        if submission.id not in already_done and has_joke:
-            r.reply("Reading you clear!")
+        comment = submission.comments[0]
+        author = comment.author
+        have_i_posted = any(string in author for string in botname)
+        
+        if submission.id not in already_done and has_joke and not have_i_posted:
+            submission.add_comment("Reading you clear!")
+            already_done.append(submission.id)
     
     
